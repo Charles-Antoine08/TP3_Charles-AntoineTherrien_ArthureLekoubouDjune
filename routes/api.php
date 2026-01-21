85cd70140e4df27d36b236298e48be38ba0d875a
Arthure-code <2393288@cegeplimoilou.ca>
2026-01-21 00:30:59 -0500

    Ajout des routes API d’authentification (signup, signin, signout)

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+// Authentication Routes with rate limiting
+Route::middleware(['throttle:5,1'])->group(function () {
+    Route::post('/signup', [AuthController::class, 'register']);
+    Route::post('/signin', [AuthController::class, 'login']);
+    Route::post('/signout', [AuthController::class, 'logout'])
+        ->middleware('auth:sanctum');
+});
