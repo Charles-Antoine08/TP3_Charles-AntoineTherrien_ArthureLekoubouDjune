46ee21683306d6f9b6e9406c2712cc320cb38533
Arthure-code <2393288@cegeplimoilou.ca>
2026-01-23 01:28:04 -0500

    Extraction de la validation createCritic dans une classe Validator dédiée

--- app/GraphQL/Mutations/CreateCritic.php
+++ app/GraphQL/Mutations/CreateCritic.php
@@ -2,41 +2,38 @@
 
 namespace App\GraphQL\Mutations;
 
-use Illuminate\Support\Facades\{Auth, Validator};
+use App\CreateCriticValidator;
 use App\Models\{Critic, FilmStatistic};
+use Illuminate\Support\Facades\{Auth, Validator};
 
 class CreateCritic
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __invoke($_, array $args)
+    public function __invoke($_, array $args): Critic
     {
-        Validator::make($args, [
-            'film_id' => 'required|exists:films,id',
-            'score'   => 'required|numeric|min:0|max:10',
-            'comment' => 'required|string',
-        ])->validate();
+        CreateCriticValidator::validate($args);
 
-        Critic::create([
+        $critic = Critic::create([
             'film_id' => $args['film_id'],
             'score'   => $args['score'],
             'comment' => $args['comment'],
             'user_id' => Auth::id(),
         ]);
 
+
         $stats = FilmStatistic::where('film_id', $args['film_id'])->first();
 
-        $totalVotes = $stats->votes + 1;
-        $newScore = (
-            ($stats->score * $stats->votes) + $args['score']
-        ) / $totalVotes;
+        if ($stats) {
+            $totalVotes = $stats->votes + 1;
+            $newScore = (
+                ($stats->score * $stats->votes) + $args['score']
+            ) / $totalVotes;
 
-        $stats->update([
-            'score' => round($newScore, 2),
-            'votes' => $totalVotes,
-        ]);
+            $stats->update([
+                'score' => round($newScore, 2),
+                'votes' => $totalVotes,
+            ]);
+        }
 
-        return $stats;
+        return $critic;
     }
 }
